9a28aad4dc57f55df8cbb124e690566443d473e2
Mustafa Alimiyaji <mustafaalimiyaji@gmail.com>
2025-06-13 18:46:46 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,8 +2,6 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import os
 from dotenv import load_dotenv
-import threading
-import time
 
 # Load environment variables
 load_dotenv()
@@ -26,12 +24,3 @@ async def health_check():
 @app.get("/api/message")
 async def get_message():
     return {"message": "You've successfully integrated the backend!"}
-
-
-def burn_cpu():
-    while True:
-        print("Burning CPU...")
-        _ = [x**2 for x in range(10000)]
-        time.sleep(0.1)
-
-threading.Thread(target=burn_cpu, daemon=True).start()
