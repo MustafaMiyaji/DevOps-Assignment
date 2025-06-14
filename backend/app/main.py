e434966372567c7e462c53af495b32dc5cbc9c8e
Mustafa Alimiyaji <mustafaalimiyaji@gmail.com>
2025-06-14 06:55:45 +0530

    Update main.py

--- backend/app/main.py
+++ backend/app/main.py
@@ -2,6 +2,9 @@ from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 import os
 from dotenv import load_dotenv
+import boto3
+import json
+import os
 
 # Load environment variables
 load_dotenv()
@@ -24,3 +27,17 @@ async def health_check():
 @app.get("/api/message")
 async def get_message():
     return {"message": "You've successfully integrated the backend!"}
+
+def get_secret():
+    secret_name = os.getenv("SECRET_ARN")
+    region_name = "ap-south-1"
+
+    session = boto3.session.Session()
+    client = session.client(service_name='secretsmanager', region_name=region_name)
+
+    response = client.get_secret_value(SecretId=secret_name)
+    return json.loads(response['SecretString'])
+
+# Use the secret
+creds = get_secret()
+print("🔐 API Key from Secrets Manager:", creds["api_key"])
